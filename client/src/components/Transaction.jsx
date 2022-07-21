f68c4be1bfa0259a060419d99bd746d2550f5a93
maniamartial <martialmania19@gmail.com>
2022-07-21 22:52:46 +0300

    creating service container

--- client/src/components/Transaction.jsx
+++ client/src/components/Transaction.jsx
@@ -1,8 +1,21 @@
-
-
-const Transaction=()=>{
-    return (
-        <h1>Transaction</h1>
-    )
-    }
-    export default Transaction;
+import React, { useContext } from "react";
+import { TransactionContext } from "../context/TransactionContext";
+const Transaction = () => {
+  const { currentAccount } = useContext(TransactionContext);
+  return (
+    <div className="flex w-full justify-center items-center 2xl:px-20 gradient-bg-transactions">
+      <div className="flex flex-col md:p-12 py-12 px-4">
+        {currentAccount ? (
+          <h3 className="text-white text-3xl text-center my-2">
+            Latest Transactions{" "}
+          </h3>
+        ) : (
+          <h3 className="text-white text-3xl text-center my-2">
+            Connect Your Account To See The Latest Transactions{" "}
+          </h3>
+        )}
+      </div>
+    </div>
+  );
+};
+export default Transaction;
---
+++ client/src/utils/shortenAddress.js
@@ -0,0 +1,2 @@
+export const shortenAddress = (address) =>
+  `${address.slice(address.length - 4)}`;
